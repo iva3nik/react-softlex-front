8c71513ce2997d3976528edbd22d89348ee077d3
Ivan Nikitin <iva3nik@yandex.ru>
2022-04-13 11:55:20 +0300

    feat: add buttons for tasks

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import cn from "classnames";
 
 import s from "./Task.module.scss";
 
 const Task = (props) => {
+  const [name, setName] = useState("admin");
   return (
     <div className={s.task}>
+      <div className={s.task__buttons}>
+        <button
+          className={cn(s.task__button, {
+            [s.task__buttons_type_admin]: name === "admin",
+          })}
+        >
+          Edit task
+        </button>
+        <button className={s.task__button}>Delete task</button>
+      </div>
       <h2 className={s.task__title}>Название задачи : {props.title}</h2>
       <p className={s.taks__text}>Текст задачи : {props.text}</p>
       <p className={s.taks__text}>Статус выполнения : {props.status}</p>
